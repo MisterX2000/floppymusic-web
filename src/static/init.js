c4254a222b01eb0d9dbfb1b6f19a9606fe457f6a
Martin Schulz <MartinSchulz2000@gmail.com>
2017-05-22 21:13:31 +0200

    add delete confirmation

--- src/static/init.js
+++ src/static/init.js
@@ -1,3 +1,7 @@
 $(function () {
     $('[data-toggle="popover"]').popover()
+    $('[data-toggle=confirmation]').confirmation({
+        rootSelector: '[data-toggle=confirmation]',
+        // other options
+    })
 })
