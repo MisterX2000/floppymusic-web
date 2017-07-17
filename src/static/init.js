c0b5f4b4ae8cc0334c99f7638eacb7181872b20e
Martin Schulz <MartinSchulz2000@gmail.com>
2017-07-18 01:07:00 +0200

    small fixes, optimizations

--- src/static/init.js
+++
@@ -1,7 +0,0 @@
-$(function () {
-    $('[data-toggle="popover"]').popover()
-    $('[data-toggle=confirmation]').confirmation({
-        rootSelector: '[data-toggle=confirmation]',
-        // other options
-    })
-})
